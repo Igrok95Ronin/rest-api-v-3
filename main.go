dad2aee2a418945b215e9885a26cb9d1a52c08d9
xl92@DESKTOP-F8KSG17 <xl92@mail.ru>
2023-04-07 21:29:40 +0300

    Обработка Url-запросов

--- main.go
+++ main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Привет из Snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Отображения выбранной заметки %d", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Метод запрещен!", 405)
+		return
+	}
+
+	w.Write([]byte("Форма для создания новый заметки..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
